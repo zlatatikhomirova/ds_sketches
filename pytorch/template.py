cfcdb25bf93fb42cbc40226a37127a6905204636
zlatatikhomirova <72922674+zlatatikhomirova@users.noreply.github.com>
2024-02-18 17:13:57 +0700

    Update template.py

--- pytorch/template.py
+++ pytorch/template.py
@@ -42,6 +42,7 @@ test
   - ~10-20%	исходных данных
   - обязательная часть разбиения
 """
+
 def split_data(data: torch.Tensor, 
                train_size: float|None=None, 
                valid_split: float|None=None,
@@ -62,5 +63,29 @@ def split_data(data: torch.Tensor,
                                                         random_state=random_state)
     return X_train, X_vaild, X_test, y_train, y_valid, y_test
   return X_train, X_test, y_train, y_test
-                 
+
 ## 2. Построение модели
+
+class LinearRegressionModel(nn.Module): # <- практически все в PyTorch это nn.Module (можно представлять это как конструктор)
+    def __init__(self):
+        super().__init__() 
+        self.weights = nn.Parameter(torch.randn(1, dtype=torch.float),
+                                   requires_grad=True) # <- для возможности вычисления градиентов
+
+        self.bias = nn.Parameter(torch.randn(1, dtype=torch.float),
+                                requires_grad=True) 
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor: 
+        return self.weights * x + self.bias # <- линейная регрессия (y = m*x + b)
+
+Строительные блоки в PyTorch
+В PyTorch есть примерно четыре основных модуля, с помощью которых можно создать практически любую нейронную сеть, какую только можно себе представить.
+
+Это torch.nn, torch.optim, torch.utils.data.Dataset and torch.utils.data.DataLoader. Сейчас мы сосредоточимся на первых двух, а к остальным перейдем позже.
+
+Модуль PyTorch	Что он делает?
+torch.nn	Содержит все строительные блоки для вычислительных графов (по сути, серии вычислений, выполняемых определенным образом).
+torch.nn.Parameter	Хранит тензоры, которые могут быть использованы с nn.Module. Если requires_grad=True, то градиенты (используемые для обновления параметров модели с помощью градиентного спуска вычисляются автоматически, что часто называют автоматическим дифференцированием.
+torch.nn.Module	Базовый класс для всех модулей нейронных сетей, все строительные блоки для нейронных сетей являются подклассами. Если вы строите нейронную сеть в PyTorch, ваши модели должны быть подклассами nn.Module. Требует реализации метода forward().
+torch.optim	Содержит различные алгоритмы оптимизации (они указывают параметрам модели, хранящимся в nn.Parameter, как лучше сдвинуться чтобы улучшить качество предсказаний модели).
+def forward()	Все подклассы nn.Module требуют метод forward(), который определяет вычисления, которые будут происходить на данных, переданных конкретному nn.Module (например, как в классе линейной регрессии выше).
